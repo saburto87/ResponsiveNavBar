635b8ab107e22d71a67d0b15d37c3175c87f3978
Sebastian <sebastian@MacBook-Air-de-Sebastian.local>
2019-05-16 18:38:10 -0400

    cambio en el js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const navSlide = () => {
-    const burger = document.querySelector('bt');
+    const burger = document.querySelector('.menu_bar');
     const nav = Document.querySelector('.nav-links');
     burger.addEventListener('click', () => {
         nav.ClassList.toggle('.nav-active');
