320293269729597838b8b53abe4e02f8f2879c7c
Sebastian <sebastian@MacBook-Air-de-Sebastian.local>
2019-05-16 14:12:20 -0400

    cambio en el nav

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const navSlide = () => {
-    const burger = document.querySelector('.bt-menu');
+    const burger = document.querySelector('bt');
     const nav = Document.querySelector('.nav-links');
     burger.addEventListener('click', () => {
-        nav.ClassList.toggle('nav-active');
+        nav.ClassList.toggle('.nav-active');
     });
 }
 navSlide();
